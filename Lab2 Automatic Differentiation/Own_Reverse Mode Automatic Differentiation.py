22006f48db1a9439f1dbc24b944c0fb171a1ffbc
Hang ZHONG <38694340+HangZhongZH@users.noreply.github.com>
2019-07-20 13:57:01 +0100

    Add files via upload

--- Lab2 Automatic Differentiation/Own_Reverse Mode Automatic Differentiation.py
+++ Lab2 Automatic Differentiation/Own_Reverse Mode Automatic Differentiation.py
@@ -1,2 +1,15 @@
-import torch
 import math
+
+
+class Var:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+        self.grad_value = None
+
+    def grad(self):
+        if self.grad_value is None:
+            self.grad_value = sum(weight * var.grad() for weight, var in self.children)
+        return self.grad_value
+
+    def 
